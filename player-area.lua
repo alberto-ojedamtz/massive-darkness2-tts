63e70add111f16428ad3a21f2097607a8df9ffc9
Alberto Ojeda <alberto.ojedamartinez@outlook.com>
2022-06-20 13:20:14 -0600

    Sets up player area relative to boards
    Player boards where added to TTS mod to have a fixed position and
    the selected characters' pieces are places relative to these.

--- player-area.lua
+++ player-area.lua
@@ -1,84 +1,57 @@
-PLAYER_BOARDS_POSITIONS = { 
-    { x = -52, y = 1, z = -44 },
-    { x = 21, y = 1, z = -44 }
+PLAYER_BOARD_GUIDS = {
+    "e0e32e",
+    "6af186",
+    "fdd3b2",
+    "5038c2"
 }
 
-LEVEL_BOARD_RELATIVE_POSITIONS = {
-    x = 4.98,
-    y = 0.12,
-    z = -1.6
-}
+LEVEL_BOARD_RELATIVE_POSITIONS = { x = 4.98, y = 0.12, z = -1.6 }
 
-CHARACTER_CARD_RELATIVE_POSITION = {
-    x = -1.24, 
-    y = 0.12, 
-    z = 1.82
-}
+CHARACTER_CARD_RELATIVE_POSITION = { x = -1.24, y = 0.12, z = 1.82 }
+
+FIGURINE_RELATIVE_POSITION = { x = -4.24, y = 0, z = 5.82 }
+
+COMPONENTS_BAG_RELATIVE_POSITION = { x = 0, y = 0, z = 5.82 }
 
 function setUpPlayerAreas()
     for _, playerMenu in pairs(PLAYER_MENUS) do
         if (playerMenu.selectedClassId > 0 and playerMenu.selectedCharacterId > 0) then
-            placePlayerBoard(playerMenu)
-            Wait.frames(function() placeLevelBoard(playerMenu) end, 5)
-            Wait.frames(function() placeCharacterOnPlayerBoard(playerMenu) end, 5)
-            Wait.frames(function() placeCharacterComponents(playerMenu) end, 5)
+            placeCharacterCardOnPlayerArea(playerMenu)
+            placeFigurineOnPlayerArea(playerMenu)
+            placeClassComponentsOnPlayerArea(playerMenu)
         end
     end
 end
 
-function placePlayerBoard(playerMenu) 
-    local boardPosition = PLAYER_BOARDS_POSITIONS[playerMenu.number]
+function placeObjectRelativeToPlayerBoard(playerMenu, object, relativeValues, lock)
+    local position = getObjectFromGUID(PLAYER_BOARD_GUIDS[playerMenu.number]).getPosition()
 
-    local playerBoard = spawnObject({ 
-        type = "Custom_Token",
-        position = boardPosition
-    })
+    local xPos = position.x + relativeValues.x
+    local yPos = position.y + relativeValues.y
+    local zPos = position.z + relativeValues.z
 
-    playerBoard.setCustomObject({ image = PLAYER_BOARD_URL })
-    playerBoard.setScale({ x = 2.6, y = 1, z = 2.6 })
-    playerBoard.setRotation({ x = 0, y = 180, z = 0 })
-    playerBoard.setLock(true)
+    if (playerMenu.number > 2) then
+        object.rotate({x = 0, y = 180, z = 0})
+        xPos = xPos - (relativeValues.x * 2)
+        zPos = zPos - (relativeValues.z * 2)
+    end
+
+    object.setPosition({ x = xPos, y = yPos, z = zPos })
+    object.interactable = true
+    object.setLock(lock)
 end
 
-function placeLevelBoard(playerMenu)
-    local playerBoardPosition = PLAYER_BOARDS_POSITIONS[playerMenu.number]
-    local xPos = playerBoardPosition.x + LEVEL_BOARD_RELATIVE_POSITIONS.x
-    local yPos = playerBoardPosition.y + LEVEL_BOARD_RELATIVE_POSITIONS.y
-    local zPos = playerBoardPosition.z + LEVEL_BOARD_RELATIVE_POSITIONS.z
-
-    local levelBoard = spawnObject({
-        type = "Custom_Token",
-        position = { x = xPos, y = yPos, z = zPos }
-    })
-
-    levelBoard.setCustomObject({ image = LEVEL_BOARD_URL, thickness = 0.1 })
-    levelBoard.setScale({ x = 0.74, y = 1, z = 0.75 })
-    levelBoard.setRotation({ x = 0, y = 180, z = 0 })
-    levelBoard.setLock(true)
+function placeCharacterCardOnPlayerArea(playerMenu)
+    local characterCard = playerMenu.selectedCharacterCard
+    placeObjectRelativeToPlayerBoard(playerMenu, characterCard, CHARACTER_CARD_RELATIVE_POSITION, true)
 end
 
-function placeCharacterOnPlayerBoard(playerMenu)
-    local position = PLAYER_BOARDS_POSITIONS[playerMenu.number]
-    local xPos = position.x + CHARACTER_CARD_RELATIVE_POSITION.x
-    local yPos = position.y + CHARACTER_CARD_RELATIVE_POSITION.y
-    local zPos = position.z + CHARACTER_CARD_RELATIVE_POSITION.z
-    playerMenu.selectedCharacterCard.setPosition({ x = xPos, y = yPos, z = zPos })
-    playerMenu.selectedCharacterCard.interactable = true
-    playerMenu.selectedCharacterCard.setLock(true)
-
-    xPos = position.x + CHARACTER_CARD_RELATIVE_POSITION.x - 3
-    yPos = position.y + CHARACTER_CARD_RELATIVE_POSITION.y
-    zPos = position.z + CHARACTER_CARD_RELATIVE_POSITION.z + 4
-    playerMenu.selectedCharacterFigurine.setPosition({ x = xPos, y = yPos, z = zPos })
-    playerMenu.selectedCharacterFigurine.interactable = true
+function placeFigurineOnPlayerArea(playerMenu)
+    local figurine = playerMenu.selectedCharacterFigurine
+    placeObjectRelativeToPlayerBoard(playerMenu, figurine, FIGURINE_RELATIVE_POSITION, false)
 end
 
-function placeCharacterComponents(playerMenu)
+function placeClassComponentsOnPlayerArea(playerMenu)
     local componentsBag = CLASS_COMPONENTS[playerMenu.selectedClassText]
-    local position = PLAYER_BOARDS_POSITIONS[playerMenu.number]
-    local xPos = position.x + CHARACTER_CARD_RELATIVE_POSITION.x
-    local yPos = position.y + CHARACTER_CARD_RELATIVE_POSITION.y
-    local zPos = position.z + CHARACTER_CARD_RELATIVE_POSITION.z + 4
-
-    componentsBag.setPosition({ x = xPos, y = yPos, z = zPos })
+    placeObjectRelativeToPlayerBoard(playerMenu, componentsBag, COMPONENTS_BAG_RELATIVE_POSITION, false)
 end
